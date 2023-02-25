dff50c00501ba9c63bbfd5dcb9e2f4d477f96761
MilesTallia <mot7506@g.rit.edu>
2023-02-25 13:42:57 -0500

    structure for screenshot func

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function screenshot() {
-    //send image
+    //change curser
+    //onclick set coord 1 and do updating screenshot preview
+    //onclickrelease set coord 2 and end preview
+    //send screenshot btwn coord 1 and 2
 }
 
 function wholePage() {
