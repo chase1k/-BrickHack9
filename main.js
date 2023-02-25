34dd6bf187ab1379927a5376feefad1594b38642
C1cada <Gabrielhwgo1@gmail.com>
2023-02-25 15:06:07 -0500

    change button

--- main.js
+++ main.js
@@ -7,18 +7,38 @@ function updateSettings() {
     
 }
 
+document.getElementById("screenshot").addEventListener("click", screenshot());
 function screenshot() {
-    //change curser
-    //onclick set coord 1 and do updating screenshot preview
-    //onclickrelease set coord 2 and end preview
-    //send screenshot btwn coord 1 and 2
+    //chrome.tabs.captureVisibleTab(null, {}, function (image) {
+//     var img = new Image();
+//     img.onload = function() {
+//         var canvas = document.createElement('canvas');
+//         canvas.width = WIDTH;
+//         canvas.height = HEIGHT;
+//         var context = canvas.getContext('2d');
+//         // Assuming px,py as starting coordinates and hx,hy be the width and the height of the image to be extracted
+//         context.drawImage(img, px, py, hx, hy, 0, 0, WIDTH, HEIGHT);
+//         var image = canvas.toDataURL('image/png');
+//         // You could deal with croppedUri as cropped image src.
+//     };
+//     img.src = image;
+// });
 }
 
+document.getElementById("wholePage").addEventListener("click", wholePage());
 function wholePage() {
-    //send whole document
+    // let capturing = chrome.tabs.captureVisibleTab();
+    // capturing.then(onCaptured, onError);
 }
 
-chrome.tabs.captureVisibleTab(null, {}, function (image) {
-   // You can add that image HTML5 canvas, or Element.
-});
+function onCaptured(imageUri) {
+    console.log(imageUri);
+  }
+  
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  
+  
+
 
