01d96d753db26be990f3f81b052d33351d4bc19a
chase1k <chasekillorin@gmail.com>
2023-02-26 09:54:18 -0500

    Still does not work

--- ImageProcessor.js
+++ ImageProcessor.js
@@ -1,35 +1,47 @@
 // import Tesseract from 'tesseract.js';
 // import { createWorker } from 'tesseract.js';
+// import * as pyTesseract from 'pytesseract';
+// import * as np from 'numpy';
+// import { Image } from 'PIL';
 
+var ProcessedText = "achoo"
 async function ImgToText(){
     //JSTesseract
-    
-    // Tesseract.recognize(
-    // 'https://tesseract.projectnaptha.com/img/eng_bw.png',
-    // 'eng',
-    // { logger: m => console.log(m) }
-    // ).then(({ data: { text } }) => {
-    // console.log(text);
-    // })
         
+    // const worker = await createWorker({
+    // logger: m => console.log(m)
+    // });
 
-    const worker = await createWorker({
-    logger: m => console.log(m)
-    });
-
-    (async () => {
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    console.log(text);
-    await worker.terminate();
-    })();
+    // (async () => {
+    // await worker.loadLanguage('eng');
+    // await worker.initialize('eng');
+    // const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    // console.log(text);
+    // await worker.terminate();
+    // })();
 
     // var ProcessedText = "text"
     // return ProcessedText;
-    ProcessedText = "text";
+    // ProcessedText = "text";
+
+    // var filename, img1, text;
+    // jsTesseract.jstesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe";
+    // filename = "backgroundTileSmall.png";
+    // img1 = np.array(Image.open(filename));
+    // text = jsTesseract.image_to_string(img1);
+
+    // Tesseract.recognize(
+    //     'https://tesseract.projectnaptha.com/img/eng_bw.png',
+    //     'eng',
+    //     { logger: m => console.log(m) }
+    //   ).then(({ data: { text } }) => {
+    //     console.log(text);
+    //   })
+
+    console.log(text);
+    let ProcessedText = "text";
+
 }
-ImgToText();
 
 function TxtToSpeech(){
 ImgToText();
