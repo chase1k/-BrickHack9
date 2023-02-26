00d324418a9df1984603d1e63517938b8c284ab7
C1cada <Gabrielhwgo1@gmail.com>
2023-02-26 11:53:07 -0500

    clean

--- screenstuff.js
+++ screenstuff.js
@@ -5,48 +5,10 @@ document.getElementById("screenshot").addEventListener("click", screenshot);
 
 function screenshot() {
     alert("hello");
-    //chrome.tabs.captureVisibleTab(null, {}, function (image) {
-//     var img = new Image();
-//     img.onload = function() {
-//         var canvas = document.createElement('canvas');
-//         canvas.width = WIDTH;
-//         canvas.height = HEIGHT;
-//         var context = canvas.getContext('2d');
-//         // Assuming px,py as starting coordinates and hx,hy be the width and the height of the image to be extracted
-//         context.drawImage(img, px, py, hx, hy, 0, 0, WIDTH, HEIGHT);
-//         var image = canvas.toDataURL('image/png');
-//         // You could deal with croppedUri as cropped image src.
-//     };
-//     img.src = image;
-// });
 }
 document.getElementById("wholePage").addEventListener("click", wholePage);
-// var el = document.getElementById("wholePage");
-// if(el){
-//   el.addEventListener("click", wholePage);
-// }
 function wholePage() {
     let capturing = chrome.tabs.captureVisibleTab(null, {}, function (image) {
-            // var img = new Image();
-            // img.onload = function() {
-            //     var canvas = document.createElement('canvas');
-            //     canvas.width = 50;
-            //     canvas.height = 50;
-            //     var context = canvas.getContext('2d');
-            //     // Assuming px,py as starting coordinates and hx,hy be the width and the height of the image to be extracted
-            //     // context.drawImage(img, px, py, hx, hy, 0, 0, WIDTH, HEIGHT);
-            //     var image = canvas.toDataURL('image/png');
-            //     // You could deal with croppedUri as cropped image src.
-            // };
-            // downloadURI(image, "ss.jpeg")
-            // chrome.downloads.download({
-            //     filename: 'screenshot.png',
-            //     url: image
-            // }, (downloadId) => {
-            //     senderResponse({success: true})
-            // })
-            // document.getElementById("iamge").src = image;
-            // img.src = image;
             alert(image);
             // ImgToText(image);
         });
@@ -57,10 +19,6 @@ function wholePage() {
 
 function onCaptured(imageUri) {
     alert(imageUri)
-    // convertURIToImageData(imageUri).then(function(imageData) {
-    //     // Here you can use imageData
-    //     alert(imageData);
-    //   });
 }
   
 function onError(error) {
@@ -86,32 +44,6 @@ function convertURIToImageData(URI) {
 
 
 async function ImgToText(URI){
-    //JSTesseract
-    
-    // Tesseract.recognize(
-    // 'https://tesseract.projectnaptha.com/img/eng_bw.png',
-    // 'eng',
-    // { logger: m => console.log(m) }
-    // ).then(({ data: { text } }) => {
-    // console.log(text);
-    // })
-        
-
-    // const worker = await createWorker({
-    // logger: m => console.log(m)
-    // });
-
-    // (async () => {
-    // await worker.loadLanguage('eng');
-    // await worker.initialize('eng');
-    // const { data: { text } } = await worker.recognize(URI);
-    // console.log(text);
-    // alert(text);
-    // await worker.terminate();
-    // })();
-
-    // var ProcessedText = "text"
-    // return ProcessedText;
     ProcessedText = "text";
 }
 ImgToText();
