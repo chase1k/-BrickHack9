107a4b7790b277645b68541e0edfe224fa892e5c
MilesTallia <mot7506@g.rit.edu>
2023-02-26 09:47:49 -0500

    stuff to do at a later date ):

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function updateSettings() {
 
     // Get the checkboxes
     var contrast = document.getElementById("contrast-check");
-    var synonyms = document.getElementById("synonyms-check");
-    var summary = document.getElementById("summary-check");
+    // var synonyms = document.getElementById("synonyms-check");
+    // var summary = document.getElementById("summary-check");
     var tts = document.getElementById("tts-check");
 
     // do shit with that
@@ -29,10 +29,10 @@ function updateSettings() {
     chrome.storage.local.set({ contrast: contrast }).then(() => { console.log("contrast(local) is set to " + value); });
     // chrome.storage.sync.set({ contrast: contrast }).then(() => { console.log("contrast(sync) is set to " + value); });
     
-    chrome.storage.local.set({ synonyms: synonyms }).then(() => { console.log("synonyms(local) is set to " + value); });
+    // chrome.storage.local.set({ synonyms: synonyms }).then(() => { console.log("synonyms(local) is set to " + value); });
     // chrome.storage.sync.set({ synonyms: synonyms }).then(() => { console.log("synonyms(sync) is set to " + value); });
     
-    chrome.storage.local.set({ summary: summary }).then(() => { console.log("summary(local) is set to " + value); });
+    // chrome.storage.local.set({ summary: summary }).then(() => { console.log("summary(local) is set to " + value); });
     // chrome.storage.sync.set({ summary: summary }).then(() => { console.log("summary(sync) is set to " + value); });
     
     chrome.storage.local.set({ tts: tts }).then(() => { console.log("tts(local) is set to " + value); });
@@ -74,14 +74,14 @@ function displayText(inputText) {
     const styling = getStyleText();
     const text = inputText;
 
-    if (localStorage["synonyms"].checked == true){
-        //text = <function that changes synonyms>(text);
-    }
+    // if (localStorage["synonyms"].checked == true){
+    //     //text = <function that changes synonyms>(text);
+    // }
 
-    if (localStorage["summary"].checked == true){
+    // if (localStorage["summary"].checked == true){
         
-        //text = <function that sumarizes test>(text);
-    }
+    //     //text = <function that sumarizes test>(text);
+    // }
 
     if (localStorage["tts"].checked == true){
         //figure out tts
